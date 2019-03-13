70dfd8cec3a21570a02e56faf2156e704f071e18
lilyhuong <44270876+lilyhuong@users.noreply.github.com>
2019-03-13 15:30:12 +0100

    Add files via upload

---
+++ Table_de_multiplication.py
@@ -0,0 +1,25 @@
+fic = open ("table_de_multiplication.txt" , "w")
+fic.write (" Table de multiplication de 2"
+           "01 x 02 = 2"
+           "02 x 02 = 4"
+           "03 x 02 = 8"
+           "04 x 02 = 10"
+           "05 x 02 = 12"
+           "06 x 02 = 13"
+           "07 x 02 = 14"
+           "08 x 02 = 16"
+           "09 x 02 = 18"
+           "Table de multiplication de 3"
+           "01 x 03 = 3"
+           "02 x 03 = 6"
+           "03 x 03 = 9"
+           "04 x 03 = 12"
+           "05 x 03 = 15"
+           "06 x 03 = 18"
+           "07 x 03 = 21"
+           "08 x 03 = 24"
+           "09 x 03 = 27" )
+
+fic.close ()
+
+           
